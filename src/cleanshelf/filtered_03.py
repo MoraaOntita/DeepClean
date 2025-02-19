dbed1cfaa2b17eaf9c4a23d6e509e4461d74b987
MoraaOntita <moraaontita1844@gmail.com>
2025-02-19 10:12:29 +0300

    Added the function to automatically create the filterd folder in the artifacts folder. Modified the name of the products to map it well

--- src/cleanshelf/filtered_03.py
+++ src/cleanshelf/filtered_03.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 
 # Load the dataset
@@ -7,8 +8,13 @@ data = pd.read_csv(file_path)
 # Filter the data for suppliers from ALKHEMY BRANDS LIMITED
 filtered_data = data[data['Supp_Name'] == 'ALKHEMY BRANDS LIMITED']
 
-# Save the filtered data
+# Define output path
 output_path = 'artifacts/cleanshelf/filtered/filtered_data.csv'
+
+# Ensure the directory exists
+os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+# Save the filtered data
 filtered_data.to_csv(output_path, index=False)
 
 print(f"Filtered data saved to: {output_path}")
--- src/cleanshelf/product_description_09.py
+++ src/cleanshelf/product_description_09.py
@@ -15,24 +15,42 @@ df = pd.read_csv(input_file)
 # Define the mapping of old product names to new names
 product_mapping = {
     "MIKALLA MAYONNAISE T-MENT 261GM": "Mikalla Mayonnaise Treatment 261g",
-    "MIKALLA STYLING GEL 150GM": "Mikalla Styling Gel 150g",
+
+
     "MIKALLA QUADRA OIL TREAT 150G": "Mikalla Quadra Oil Treatment 150g",
-    "MIKALLA QUADRA OIL TREAT 250G": "Mikalla Quadra Oil Treatment 250g",
-    "MIKALLA HNY&BUTTER COND 450ML": "Mikalla Honey & Butter Conditioner 450ml",
-    "MIKALLA CLEANS&COND SHAMP 450ML": "Mikalla Shampoo 450ml",
+    "MIKALLA QUADRA OIL TREAT 250G": "Mikalla Quadra Oil Treatment 250g",    
+    "MIKALLA QUADRA OIL TREAT 400G": "Mikalla Quadra Oil Treatment 400g",  
+
+
     "MIKALLA MOIST BOOSTLEAVE 100ML": "Mikalla Leave-In Treatment 100ml",
     "MIKALLA MOIST BOOSTLEAVE 250ML": "Mikalla Leave-In Treatment 250ml",
-    "MIKALLA MOIST BOOSTLEAVE 450ML": "Mikalla Leave-In Treatment 450ml",
-    "MIKALLA NOURISHING H/FOOD 75G": "Mikalla Scalp Soother Hairfood 75g",
-    "MIKALLA ANTI-DANDRUFF CRM 150G": "Mikalla Scalp Soother Hairfood 150g",
-    "MIKALLA ANTI-DANDRUFF CRM 75G": "Mikalla Scalp Soother Hairfood 75g",
-    "MIKALLA 5IN1 BRAID SPRAY 120ML": "Mikalla 5in1 Braid Spray 120ml",
-    "MIKALLA 5IN1 BRAID SPRAY 250ML": "Mikalla 5in1 Braid Spray 250ml",
-    "MIKALLA QUADRA OIL TREAT 400G": "Mikalla Quadra Oil Treatment 250g",
-    "MIKALLA HONEY&BUTTER BERRY FUSION BODY SCRUB 300GM ": "H&B Berry Fusion Scrub 300g",
-    "MIKALLA HONEY&BUTTER COFFEE&VANILLA BODY SCRUB 300GM": "H&B Coffee Vanilla Scrub 300g",
+    "MIKALLA MOIST BOOSTLEAVE 450ML": "Mikalla Leave-In Treatment 450ml",      
+    
+
+    "MIKALLA STYLING GEL 150GM": "Mikalla Styling Gel 150g",
+
+    "MIKALLA HNY&BUTTER COND 450ML": "Mikalla Honey & Butter Conditioner 450ml",
+
+
+    "MIKALLA CLEANS&COND SHAMP 450ML": "Mikalla Shampoo 450ml",
+
+    "MIKALLA NOURISHING H/FOOD 75G": "Mikalla Nourishing Hairfood 75g",
+    
     "MIKALLA SCALP SOOTHER 150GM ": "Mikalla Scalp Soother Hairfood 150g",
     "MIKALLA SCALP SOOTHER 75GM ": "Mikalla Scalp Soother Hairfood 75g",
+
+
+    "MIKALLA ANTI-DANDRUFF CRM 150G": "Mikalla Anti-dandruff Cream 150g",
+    "MIKALLA ANTI-DANDRUFF CRM 75G": "Mikalla Anti-dandruff Cream 75g",
+
+
+    "MIKALLA 5IN1 BRAID SPRAY 120ML": "Mikalla 5in1 Braid Spray 120ml",
+    "MIKALLA 5IN1 BRAID SPRAY 250ML": "Mikalla 5in1 Braid Spray 250ml",
+
+    
+    "MIKALLA HONEY&BUTTER BERRY FUSION BODY SCRUB 300GM ": "H&B Berry Fusion Scrub 300g",
+    "MIKALLA HONEY&BUTTER COFFEE&VANILLA BODY SCRUB 300GM": "H&B Coffee Vanilla Scrub 300g",
+
 }
 
 # Apply the mapping to the 'Product_Desc' column
